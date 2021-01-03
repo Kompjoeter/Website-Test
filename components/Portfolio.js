c7bd17fc2824ee795d9328cd4ba51640e9af4925
Joran de Boer <47929945+RanDByyp@users.noreply.github.com>
2021-01-03 15:38:23 +0100

    Added selected project overlay

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -5,7 +5,8 @@ class Portfolio extends React.Component
         super(props);
         this.state = 
         {
-            projectCategory: 'all'
+            projectCategory: 'all',
+            overlayEnabled: false
         }
     }
 
@@ -15,13 +16,45 @@ class Portfolio extends React.Component
         {
             projectCategory: e.target.value
         });
-        
     }
+
+    onClickEvent = (title) =>
+    {
+
+        this.setState( 
+        {
+            overlayEnabled: true
+        });
+        console.log(title);
+        document.getElementById("overlay-project-title").innerText = title;
+    }
+
+        
+    disableOverlay = () =>
+    {
+        this.setState( 
+        {
+            overlayEnabled: false
+        });
+    }
+
 
     render()
     {
         return(
             <>
+
+                <div className={[true && 'project-overlay', this.state.overlayEnabled && 'active', ]
+                    .filter(c => c != false)
+                    .join(' ')}>
+                    <button onClick={this.disableOverlay} class="project-overlay-exit">X</button>
+                    <div class="project-overlay-content-container">
+                        <div class="project-overlay-content">
+                        <h2 id="overlay-project-title">Project Name</h2>
+                        <p id="overlay-project-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem doloribus omnis quod voluptatum. Cum, distinctio. Accusamus ea saepe fugit amet tenetur nesciunt cupiditate deserunt sit placeat reiciendis quaerat eum voluptate labore, suscipit, veniam adipisci! Minus laudantium consectetur excepturi deleniti! Sit.</p>
+                        </div>
+                    </div>
+                </div>
                 <div class="portfolio-menu">
                     <button onClick={this.categorySet} value="all">All</button>
                     <button onClick={this.categorySet} value="html">HTML</button>
@@ -32,15 +65,15 @@ class Portfolio extends React.Component
                     <button onClick={this.categorySet} value="json">JSON</button>
                 </div>
                 <div class="portfolio-content">
-                    <Project filterBy={this.state.projectCategory} title={'Project Alpha'} tags={"html css"}/>
-                    <Project filterBy={this.state.projectCategory} title={'Project Beta'} tags={"html css javascript"}/>
-                    <Project filterBy={this.state.projectCategory} title={'Project Charlie'} tags={"html css javascript react"}/>
-                    <Project filterBy={this.state.projectCategory} title={'Project Dingo'} tags={"javascript"}/>
-                    <Project filterBy={this.state.projectCategory} title={'Project Eagle'} tags={"html"}/>
-                    <Project filterBy={this.state.projectCategory} title={'Project Foxtrot'} tags={"html css javascript angular"}/>
-                    <Project filterBy={this.state.projectCategory} title={'Project Garage'} tags={"html css javascript json"}/>
-                    <Project filterBy={this.state.projectCategory} title={'Project Houndog'} tags={"html css"}/>
-                    <Project filterBy={this.state.projectCategory} title={'Project Ingrid'} tags={"html css javascript json"}/>
+                    <Project title={'Project Alpha'} tags={"html css"} onSelect={this.onClickEvent} filterBy={this.state.projectCategory}  />
+                    <Project title={'Project Bravo'} tags={"html css javascript"} onSelect={this.onClickEvent} filterBy={this.state.projectCategory} />
+                    <Project title={'Project Charlie'} tags={"html css javascript react"} onSelect={this.onClickEvent} filterBy={this.state.projectCategory}/>
+                    <Project title={'Project Dingo'} tags={"javascript"} onSelect={this.onClickEvent} filterBy={this.state.projectCategory}/>
+                    <Project title={'Project Echo'} tags={"html"} onSelect={this.onClickEvent} filterBy={this.state.projectCategory} />
+                    <Project title={'Project Foxtrot'} tags={"html css javascript angular"} onSelect={this.onClickEvent} filterBy={this.state.projectCategory}/>
+                    <Project title={'Project Golf'} tags={"html css javascript json"} onSelect={this.onClickEvent} filterBy={this.state.projectCategory}/>
+                    <Project title={'Project Hotel'} tags={"html css"} onSelect={this.onClickEvent} filterBy={this.state.projectCategory} />
+                    <Project  title={'Project India'} tags={"html css javascript json"} onSelect={this.onClickEvent} filterBy={this.state.projectCategory}/>
                 </div>
             </>
         )
